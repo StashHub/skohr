603f6ad7987adb9b1c44145d5e17316ed7c63206
Damian Gaynor <drgayno@gmail.com>
2024-05-25 20:45:50 -0400

    chore: code cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   'ignorePatterns': ['apps/**', 'packages/**'],
-  'extends': ['@repo/eslint-config/library.js'],
+  'extends': ['@skohr/eslint-config/library.js'],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'project': true,
--- packages/utils/.eslintrc.cjs
+++ packages/utils/.eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   'root': true,
-  'extends': ['@repo/eslint-config/react.js'],
+  'extends': ['@skohr/eslint-config/react.js'],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'project': true,
