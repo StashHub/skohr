2d0819b154a37d09f12f13575e26703715cf7c54
Damian Gaynor <drgayno@gmail.com>
2024-06-16 03:45:51 -0400

    chore: minor changes

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,14 +1,11 @@
-import NextAuth from 'next-auth';
-
+import NextAuth, { Session } from 'next-auth';
 import { authOptions } from './config';
 
 export type { Session } from 'next-auth';
 
-const {
+export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth(authOptions);
-
-export { GET, POST, auth, signIn, signOut };
