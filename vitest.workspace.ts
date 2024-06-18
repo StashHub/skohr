1724e88fc5b25e1179bac5686ab93592ebd49002
Damian Gaynor <drgayno@gmail.com>
2024-06-18 04:44:29 -0400

    fix: vitest setup

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,19 +2,5 @@ import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
   'packages/*',
-  {
-    extends: './vitest.config.ts',
-    test: {
-      include: ['tests/**/*.{browser}.test.{ts,js}'],
-      name: 'happy-dom',
-      environment: 'happy-dom',
-    },
-  },
-  {
-    test: {
-      include: ['tests/**/*.{node}.test.{ts,js}'],
-      name: 'node',
-      environment: 'node',
-    },
-  },
+  { extends: './vitest.config.ts' },
 ]);
