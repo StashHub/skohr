5064a7b13b7f935e16a19320a57ca50829d68175
Damian Gaynor <drgayno@gmail.com>
2024-07-01 15:03:02 -0400

    feat: add jiti to validate .env at build time

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,21 @@
+import { fileURLToPath } from "url";
+import createJiti from "jiti";
+
+// import env files to validate at build time. Use jiti so we can load .ts files in here.
+createJiti(fileURLToPath(import.meta.url))("./src/env");
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
   /** enables hot reloading for local packages without a build step */
-  transpilePackages: ['@skohr/api', '@skohr/auth', '@skohr/db', '@skohr/ui'],
+  transpilePackages: [
+    '@skohr/api',
+    '@skohr/auth',
+    '@skohr/db',
+    '@skohr/ui',
+    '@skohr/lib'
+  ],
   images: {
     remotePatterns: [
       {
